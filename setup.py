05a394f139286c6e35382e6ca47c387248786053
harshil <coerciasink@gmail.com>
2025-08-11 21:18:44 +0530

    fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pixBoards",
-    version=2.4,
+    version=2.5,
     packages=find_packages(),
     include_package_data=True,
     package_data={"pixBoards": ["templates/*.*"]},
