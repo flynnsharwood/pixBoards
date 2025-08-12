cca825fc76e6eefc0736fac79fbb2bfa2d82d4bd
harshil <coerciasink@gmail.com>
2025-08-13 00:29:08 +0530

    fixes

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from setuptools import find_packages, setup
 with open('readme.md', 'r') as f:
     description = f.read()
 
+def parse_requirements(filename= "requirements.txt"):
+    with open(filename, encoding="utf-8") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
 setup(
     name="pixBoards",
-    version="0.2.8",
+    version="0.2.10",
     packages=find_packages(),
     include_package_data=True,
     package_data={"pixBoards": ["templates/*.*"]},
-    # install_requires = [
-    # ]
+    install_requires = parse_requirements(),
     entry_points={
         "console_scripts": [
             "run=boards.cli:main",
@@ -21,8 +24,9 @@ setup(
 )
 
 """
-git add . && git commit -m ""
+git add . && git commit -m "fixes"
 git push
 python3 setup.py sdist bdist_wheel
 twine upload dist/*
+
 """
